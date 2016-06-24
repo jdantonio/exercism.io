90434ee85abe42eca08bb6fa3f02e4e5c8c3538a
Jerry D'Antonio <stumpjumper@gmail.com>
2016-06-24 19:55:45 -0400

    Forced Bob to tap out.

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,14 +1,19 @@
-//
-// This is only a SKELETON file for the "Bob" exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-//
-// YOUR CODE GOES HERE
-//
+  var exclamation = /^(([^a-z]*[A-Z]+[^a-z]*[!?])|([A-Z\s]*[A-Z]+))$/g;
+  var question = /^.*[a-z\d].*\?$/g;
+  var silence = /^\s*$/g;
+
+  if (exclamation.test(input)) {
+    return 'Whoa, chill out!';
+  } else if (question.test(input)) {
+    return 'Sure.';
+  } else if (silence.test(input)) {
+    return 'Fine. Be that way!';
+  } else {
+    return 'Whatever.';
+  }
 };
 
 module.exports = Bob;
