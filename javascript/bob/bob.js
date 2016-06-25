60d0a9165f9f83dedaa5fc2efada75c1afc4f568
Jerry D'Antonio <stumpjumper@gmail.com>
2016-06-24 21:45:48 -0400

    Functional Bob.

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,19 +1,18 @@
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-  var exclamation = /^(([^a-z]*[A-Z]+[^a-z]*[!?])|([A-Z\s]*[A-Z]+))$/g;
-  var question = /^.*[a-z\d].*\?$/g;
-  var silence = /^\s*$/g;
 
-  if (exclamation.test(input)) {
-    return 'Whoa, chill out!';
-  } else if (question.test(input)) {
-    return 'Sure.';
-  } else if (silence.test(input)) {
-    return 'Fine. Be that way!';
-  } else {
-    return 'Whatever.';
-  }
+  var matchers = [
+    [/^[^a-z]*[A-Z]+[^a-z]*[!?]$/, 'Whoa, chill out!'],
+    [/^[A-Z\s]*[A-Z]+$/,           'Whoa, chill out!'],
+    [/^.*[a-z\d].*\?$/,            'Sure.'],
+    [/^\s*$/,                      'Fine. Be that way!'],
+    [/^\s*[^\s].*$/,               'Whatever.']
+  ]
+
+  return matchers.find(function(matcher) {
+    return matcher[0].test(input);
+  })[1];
 };
 
 module.exports = Bob;
