cb8a400351d7ec1da7d56be9d1b9e5b79e783348
Jerry D'Antonio <stumpjumper@gmail.com>
2016-06-25 15:34:10 -0400

    JavaScript beer song is more functional.

--- javascript/beer-song/beer-song.js
+++ javascript/beer-song/beer-song.js
@@ -1,13 +1,18 @@
 var BeerSong = function() {};
 
 const MANY = "{1} bottles of beer on the wall, {1} bottles of beer.\n\
-Take one down and pass it around, {0} of beer on the wall.\n"
+Take one down and pass it around, {0} bottles of beer on the wall.\n"
+
+const TWO = "2 bottles of beer on the wall, 2 bottles of beer.\n\
+Take one down and pass it around, 1 bottle of beer on the wall.\n"
 
 const ONE = "1 bottle of beer on the wall, 1 bottle of beer.\n\
 Take it down and pass it around, no more bottles of beer on the wall.\n"
 
 const NONE = "No more bottles of beer on the wall, no more bottles of beer.\n\
 Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+
+const LYRICS = [NONE, ONE, TWO, MANY];
 
 function downSequence(high, low) {
   return Array.apply(0, Array(high - low + 1)).map(function(x, y) {
@@ -16,14 +21,8 @@ function downSequence(high, low) {
 }
 
 BeerSong.prototype.verse = function(verse) {
-  if (verse === 0) {
-    return NONE;
-  } else if (verse === 1) {
-    return ONE;
-  } else {
-    var last = (verse === 2 ? '1 bottle' : String(verse - 1) + ' bottles');
-    return MANY.replace(/\{1\}/g, verse).replace(/\{0\}/g, last);
-  }
+  var lyric = LYRICS[verse] || MANY; 
+  return lyric.replace(/\{1\}/g, verse).replace(/\{0\}/g, verse - 1);
 };
 
 BeerSong.prototype.sing = function(high, low) {
