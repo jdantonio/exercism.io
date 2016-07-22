42fd2a34d05f06201ce4b3344e9bd6003f2a558b
Jerry D'Antonio <stumpjumper@gmail.com>
2016-07-22 08:21:52 -0400

    Made stuff private.

--- javascript/phone-number/phone-number.js
+++ javascript/phone-number/phone-number.js
@@ -3,6 +3,14 @@
 
 const INVALID = '0000000000';
 
+function coCode(phoneNumber) {
+  return phoneNumber.substring(3, 6);
+}
+
+function line(phoneNumber) {
+  return phoneNumber.substring(6);
+}
+
 var PhoneNumber = function(phoneNumber) {
   this.phoneNumber = validate(cleanse(phoneNumber));
 };
@@ -24,16 +32,8 @@ PhoneNumber.prototype.areaCode = function() {
   return this.phoneNumber.substring(0, 3);
 };
 
-PhoneNumber.prototype.coCode = function() {
-  return this.phoneNumber.substring(3, 6);
-}
-
-PhoneNumber.prototype.line = function() {
-  return this.phoneNumber.substring(6);
-}
-
 PhoneNumber.prototype.toString = function() {
-  return '(' + this.areaCode() + ') ' + this.coCode() + '-' + this.line();
+  return '(' + this.areaCode() + ') ' + coCode(this.phoneNumber) + '-' + line(this.phoneNumber);
 };
 
 module.exports = PhoneNumber;
